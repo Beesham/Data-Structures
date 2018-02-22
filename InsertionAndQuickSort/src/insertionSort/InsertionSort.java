85ddfb30a913937610337207ea64ce450b2b0fa2
Beesham <sarendranauth.beesham@gmail.com>
2018-02-22 15:59:58 -0500

    Docs: add documentation of author

--- InsertionAndQuickSort/src/insertionSort/InsertionSort.java
+++ InsertionAndQuickSort/src/insertionSort/InsertionSort.java
@@ -1,7 +1,15 @@
 package insertionSort;
 
+
+/**
+ * Insertion sort algorithm
+ * @author Beesham Sarendranauth
+ */
 public class InsertionSort {
 
+	//Worst-case :O(n^2)
+	//Best-case :O(n^2), even if the list is sorted, the algo doesn't know
+	// that and thus still looks for unsorted items below the index
 	public static void sort(int arr[], int sizeOfArray) {
 		for(int i = 0; i < sizeOfArray; i++) {
 			for(int j = i - 1, k = i; j >= 0; j--, k--) {
