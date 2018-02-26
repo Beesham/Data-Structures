439727adf9773930c223d3765ffabdcd9f5b9802
Beesham <sarendranauth.beesham@gmail.com>
2018-02-26 18:36:21 -0500

    Edit test

---
+++ InsertionAndQuickSort/src/test/QuickSortTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import quickSort.QuickSort;
+
+public class QuickSortTest {
+	public static void main(String[] args) {
+		int arr[] = {2,5,78,9,3,87,4};
+
+		for (int i = 0; i < arr.length; i++) {
+			int i1 = arr[i];
+			System.out.print(i1 + " ");
+		}
+		System.out.println("");
+		QuickSort.sort(arr, 0, 6);
+
+		for (int i = 0; i < arr.length; i++) {
+			int i1 = arr[i];
+			System.out.print(i1 + " ");
+		}
+	}
+}
