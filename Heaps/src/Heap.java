cba7ccc0e981907cf4b09c31b8e424d13cc415a6
Beesham <sarendranauth.beesham@gmail.com>
2018-02-28 17:23:30 -0500

    Override HeapADT methods in Heap

---
+++ Heaps/src/Heap.java
@@ -0,0 +1,30 @@
+/**
+ * Heap Data Structure
+ * @author Beesham Sarendranauth
+ */
+public class Heap implements HeapADT {
+	@Override
+	public void insert(Object o) {
+		
+	}
+
+	@Override
+	public void removeMin(Object o) {
+
+	}
+
+	@Override
+	public Object min() {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+}
