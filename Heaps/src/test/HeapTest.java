635cafe73dd36d17e1d8995c6c0782820cf82d49
Beesham <sarendranauth.beesham@gmail.com>
2018-03-07 17:01:27 -0500

    Test isEmpty

--- Heaps/src/test/HeapTest.java
+++ Heaps/src/test/HeapTest.java
@@ -11,12 +11,16 @@ public class HeapTest {
 	public static void main(String[] args) {
 		Heap<Integer> heap = new Heap<Integer>();
 
-		//Element e = new Element(2);
+		System.out.println("Expected: true, Actual: " + heap.isEmpty());
+
 		heap.insert(2);
 		heap.insert(7);
 		heap.insert(1);
 
 		System.out.println(heap.min());
 		System.out.println(heap.size());
+		System.out.println("Expected: false, Actual: " + heap.isEmpty());
+
+
 	}
 }
