1adbc23f038bd4ead75037ff4090b8aa50b02b97
Beesham <sarendranauth.beesham@gmail.com>
2018-03-07 15:52:06 -0500

    Add Element class

---
+++ Heaps/src/Element.java
@@ -0,0 +1,12 @@
+/**
+ * Integer object that stores a single interger
+ * like a wrapper
+ * @author Beesham Sarendranauth
+ */
+public class Element{
+	Integer i;
+
+	public Element(int i) {
+		this.i = i;
+	}
+}
