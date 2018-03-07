469727d99cc904ba41f02df558111baf42998c92
Beesham <sarendranauth.beesham@gmail.com>
2018-03-07 16:58:09 -0500

    Update test

--- Heaps/src/test/HeapTest.java
+++ Heaps/src/test/HeapTest.java
@@ -1,4 +1,22 @@
 package test;
 
+import HeapADT.Element;
+import HeapADT.Heap;
+
+/**
+ * Tests Heap methods
+ * @author Beesham Sarendranauth
+ */
 public class HeapTest {
+	public static void main(String[] args) {
+		Heap<Integer> heap = new Heap<Integer>();
+
+		//Element e = new Element(2);
+		heap.insert(2);
+		heap.insert(7);
+		heap.insert(1);
+
+		System.out.println(heap.min());
+		System.out.println(heap.size());
+	}
 }
