ab9ea9c5c050fb52c2f23837e18bf35e3672b4b1
Beesham <sarendranauth.beesham@gmail.com>
2018-03-19 20:33:24 -0400

    Add BST ADT project

---
+++ BinarySearchTree/src/BinaryTreeADT.java
@@ -0,0 +1,10 @@
+/**
+ * @author Beesham Sarendranauth
+ * @param <E>
+ */
+public interface BinaryTreeADT<E> {
+	Object search (E o);
+	Object find (E o);
+	void insert (E o, Node n);
+	Object remove (E o);
+}
