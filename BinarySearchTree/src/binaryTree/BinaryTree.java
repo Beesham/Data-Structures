4cbe1c60146142d276773c4aa2474375325396dd
Beesham <sarendranauth.beesham@gmail.com>
2018-03-21 19:25:52 -0400

    Docs: update documentation in BinarySearchTree class

--- BinarySearchTree/src/binaryTree/BinaryTree.java
+++ BinarySearchTree/src/binaryTree/BinaryTree.java
@@ -17,7 +17,6 @@ public class BinaryTree<E extends Comparable> implements BinaryTreeADT{
 
 	@Override
 	public Object search(Comparable o) {
-		//TODO
 		Node<E> n = searchItem(o, head);
 		if(n == null) return null;
 		return n.getO();
@@ -43,33 +42,34 @@ public class BinaryTree<E extends Comparable> implements BinaryTreeADT{
 
 	@Override
 	public void insert(Comparable o) {
-		//TODO
 		insertItem(o, head);
 	}
 
 	private Node insertItem(Comparable o, Node n) {
+		//searches for an insertion point in the tree
 		Node insertionPoint = searchItem(o, n);
-		if(insertionPoint.getO() == null) {
-			return insertAtExternal(o, insertionPoint);
-		}
+
+		//Inserts the value at the found insertion point
+		if(insertionPoint.getO() == null) return insertAtExternal(o, insertionPoint);
 		if(insertionPoint.getO().compareTo(o) == 0) return insertItem(o, n.getLeft());
 		return insertionPoint;
 	}
 
 	private Node insertAtExternal(Comparable o, Node insertionPoint) {
+		//set the external node to new value
 		insertionPoint.setO(o);
+
+		//Creates blank external nodes
 		Node left = new Node(null);
 		Node right = new Node(null);
 
+		//set the external nodes' parents to the inserted node
 		left.setParent(insertionPoint);
 		right.setParent(insertionPoint);
 
+		//set the left and right children of the newly inserted node to blank external nodes
 		insertionPoint.setLeft(left);
 		insertionPoint.setRight(right);
-
-		if(head.getO() == null) {
-			head = insertionPoint;
-		}
 
 		return insertionPoint;
 	}
