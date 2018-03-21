d21eccdfc977a5ece046a9c0e2f5b58d0711ffbc
Beesham <sarendranauth.beesham@gmail.com>
2018-03-21 19:26:55 -0400

    Add cases to Main in BST

--- BinarySearchTree/src/Main.java
+++ BinarySearchTree/src/Main.java
@@ -1,9 +1,16 @@
+import binaryTree.BinaryTree;
+
 /**
  * Main class too test BST
  * @author Beesham Sarendranauth
  */
 public class Main {
 	public static void main(String[] args) {
+		BinaryTree binaryTree = new BinaryTree();
+		binaryTree.insert(2);
+		binaryTree.insert(1);
+		binaryTree.insert(3);
 
+		int a = (int) binaryTree.search(2);
 	}
 }
