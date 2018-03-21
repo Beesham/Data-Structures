807d7c787b01a003b5f5934d010ebe088b4c0892
Beesham <sarendranauth.beesham@gmail.com>
2018-03-21 19:19:17 -0400

    Feat: insert and search in BST

--- BinarySearchTree/src/binaryTree/BinaryTree.java
+++ BinarySearchTree/src/binaryTree/BinaryTree.java
@@ -20,18 +20,19 @@ public class BinaryTree<E extends Comparable> implements BinaryTreeADT{
 		//TODO
 		Node<E> n = searchItem(o, head);
 		if(n == null) return null;
-
 		return n.getO();
 	}
 
 	private Node searchItem(Comparable o, Node n) {
-		if(n == null) return null;
+		//Base case
+		if(n.getO() == null) return n;
 
-		//if(((Comparable) o).compareTo(n.getO())) {
-
-		//}
-
-		return null;
+		if(o.compareTo(n.getO()) < 0) { //If object < current object
+			return searchItem(o, n.getLeft());
+		}else if(o.compareTo(n.getO()) > 0){ //If object > current object
+			return searchItem(o, n.getRight());
+		}else
+			return n;
 	}
 
 	@Override
@@ -43,19 +44,34 @@ public class BinaryTree<E extends Comparable> implements BinaryTreeADT{
 	@Override
 	public void insert(Comparable o) {
 		//TODO
-		Node n = new Node(o);
-		n.setLeft(null);
-		n.setRight(null);
-
-
+		insertItem(o, head);
 	}
 
-	private void insertItem(Comparable o, Node n) {
-
+	private Node insertItem(Comparable o, Node n) {
+		Node insertionPoint = searchItem(o, n);
+		if(insertionPoint.getO() == null) {
+			return insertAtExternal(o, insertionPoint);
+		}
+		if(insertionPoint.getO().compareTo(o) == 0) return insertItem(o, n.getLeft());
+		return insertionPoint;
 	}
 
-	private void insertAtExternal(Comparable o, Node n) {
+	private Node insertAtExternal(Comparable o, Node insertionPoint) {
+		insertionPoint.setO(o);
+		Node left = new Node(null);
+		Node right = new Node(null);
 
+		left.setParent(insertionPoint);
+		right.setParent(insertionPoint);
+
+		insertionPoint.setLeft(left);
+		insertionPoint.setRight(right);
+
+		if(head.getO() == null) {
+			head = insertionPoint;
+		}
+
+		return insertionPoint;
 	}
 
 	@Override
