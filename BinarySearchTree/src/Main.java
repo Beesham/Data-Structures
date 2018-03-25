98de1f11e93302b57731fac7815caa0d488b08fe
Beesham <sarendranauth.beesham@gmail.com>
2018-03-24 22:50:33 -0400

    Add cases

--- BinarySearchTree/src/Main.java
+++ BinarySearchTree/src/Main.java
@@ -7,10 +7,22 @@ import binaryTree.BinaryTree;
 public class Main {
 	public static void main(String[] args) {
 		BinaryTree binaryTree = new BinaryTree();
+		binaryTree.insert(6);
 		binaryTree.insert(2);
+		binaryTree.insert(9);
 		binaryTree.insert(1);
-		binaryTree.insert(3);
+		binaryTree.insert(4);
+		binaryTree.insert(8);
+		binaryTree.insert(10);
+
+
 
 		int a = (int) binaryTree.search(2);
+		//int b = (int) binaryTree.search(5);
+
+		binaryTree.remove(6);
+		int c = (int) binaryTree.search(2);
+
+		System.out.println(a);
 	}
 }
